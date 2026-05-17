4f0db95515f3a58711ff2480be9bf5ccaab84af4
mysoo11 <blackhaired11@gmail.com>
2026-05-17 23:08:41 +0545

    Initial project setup

---
+++ src/main/java/com/svelte/dao/ProductDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.svelte.dao;
+
+/**
+ *
+ * @author PH
+ */
+public class ProductDAO {
+    
+}
---
+++ src/main/java/com/svelte/util/PasswordUtil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.svelte.util;
+
+/**
+ *
+ * @author PH
+ */
+public class PasswordUtil {
+    
+}
